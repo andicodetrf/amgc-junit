a453c71da7d29e2d8acaa3da8a6f5d6997f192ea
andicodetrf <andrea_lau92@hotmail.com>
2022-08-15 16:42:19 +0800

    sample unit test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DemoApplicationTests {
+	
+	Calculator calc = new Calculator();
 
+	//given, when, then - BDD format
+	
 	@Test
-	void contextLoads() {
+	void itShouldAddTwoNumbers() {
+		//given
+		int numberOne = 20;
+		int numberTwo = 35;
+		
+		//when
+		int result = calc.add(numberOne, numberTwo);
+		
+		//then
+		int expected = 55;
+		assertThat(result).isEqualTo(expected);
+	}
+	
+	class Calculator {
+		int add(int a, int b) {
+			return a + b;
+		}
+		
 	}
 
 }
